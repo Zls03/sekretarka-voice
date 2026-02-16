6e1e74aa25a1addb7fe0dd6a3bc6973ba65f96dc
Paweł <kin8@o2.pl>
2026-02-16 14:43:21 +0100

    update

--- bot.py
+++ bot.py
@@ -654,7 +654,7 @@ async def websocket_endpoint(websocket: WebSocket):
             punctuate=True,
             numerals=True,
             interim_results=True,
-            utterance_end_ms=1000,
+            utterance_end_ms=1200,
             endpointing=350,
             keyterm=tenant_keyterms,  # 🔥 Dynamiczne per firma!
         )
